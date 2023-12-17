8ff2876db803d8e0c941e56f87d3415807e3991a
Amman Gillani <ammangillani4321@icloud.com>
2023-12-17 11:32:27 +0500

    commit

--- setup.js
+++ setup.js
@@ -1,23 +1,34 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4wNTQslInVVxPxq4EcKQe2MR_gIbaEBM",
-    authDomain: "fir-class-smit.firebaseapp.com",
-    projectId: "fir-class-smit",
-    storageBucket: "fir-class-smit.appspot.com",
-    messagingSenderId: "182752865318",
-    appId: "1:182752865318:web:e695f1438c196f1d2c8ae7",
-    measurementId: "G-K8MN086HXV"
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyA4wNTQslInVVxPxq4EcKQe2MR_gIbaEBM",
+  authDomain: "fir-class-smit.firebaseapp.com",
+  projectId: "fir-class-smit",
+  storageBucket: "fir-class-smit.appspot.com",
+  messagingSenderId: "182752865318",
+  appId: "1:182752865318:web:e695f1438c196f1d2c8ae7",
+  measurementId: "G-K8MN086HXV",
+});
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+
+const auth = firebaseApp.auth();
+function signupcall() {
+  let email = document.getElementById("email");
+  let password = document.getElementById("password");
+  console.log(email.value);
+  console.log(password.value);
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
+}
